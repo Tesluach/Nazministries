92f641154437e74b54ab9dac81cdbb09ec444c7c
Tesluach <Tylersonluiz@gmail.com>
2019-12-28 00:59:51 +0300

    The about is added on

--- js/main.js
+++ js/main.js
@@ -10,30 +10,66 @@ let showMenu = false;
 menuBtn.addEventListener('click', toggleMenu);
 //working with the toggle menu
 function toggleMenu() {
-    if (!showMenu) {
-        hamburger.classList.add('open');
-        nav.classList.add('open');
-        menuNav.classList.add('open');
-        navItems.forEach(item => item.classList.add('open'));
+  if (!showMenu) {
+    hamburger.classList.add('open');
+    nav.classList.add('open');
+    menuNav.classList.add('open');
+    navItems.forEach((item) => item.classList.add('open'));
 
-        showMenu = true;
-    } else {
-        hamburger.classList.remove('open');
-        nav.classList.remove('open');
-        menuNav.classList.remove('open');
-        navItems.forEach(item => item.classList.remove('open'));
+    showMenu = true;
+  } else {
+    hamburger.classList.remove('open');
+    nav.classList.remove('open');
+    menuNav.classList.remove('open');
+    navItems.forEach((item) => item.classList.remove('open'));
 
-        showMenu = false;
-    }
+    showMenu = false;
+  }
 }
 
 // The javaScript effect on the scroll--------
 const header = document.querySelector('header');
 
 window.addEventListener('scroll', (e) => {
-    if (window.scrollY) {
-        header.classList.add('on_scroll');
-    } else {
-        header.classList.remove('on_scroll');
-    }
-})
+  if (window.scrollY) {
+    header.classList.add('on_scroll');
+  } else {
+    header.classList.remove('on_scroll');
+  }
+});
+
+// Working with the navigation buttons on the about page
+
+const tabs = document.querySelector('.navigate');
+const panels = document.querySelectorAll('.panel');
+const btns = document.querySelectorAll('.btn');
+
+tabs.addEventListener('click', (e) => {
+  if (e.target.tagName == 'BUTTON') {
+    const targetPanel = document.querySelector(e.target.dataset.target);
+
+    panels.forEach((panel) => {
+      if (panel == targetPanel) {
+        panel.classList.add('active');
+      } else {
+        panel.classList.remove('active');
+      }
+    });
+
+    btns.forEach((btn) => {
+      if (btn == e.target) {
+        btn.classList.add('active');
+      } else {
+        btn.classList.remove('active');
+      }
+    });
+  }
+});
+
+// btns.forEach((btn) =>
+//   btn.addEventListener('click', (e) => {
+//     if (e.target.tagName == '') {
+
+//     }
+//   })
+// );
